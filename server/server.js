580a6c67c0b3bc14c2b6ecd70757ac7cc9edfc9a
Tyreeze <wedding5@msu.edu>
2022-03-11 13:44:54 -0600

    finished setting up server

--- server/server.js
+++ server/server.js
@@ -27,8 +27,14 @@ if (process.env.NODE_ENV === 'production') {
 
 //app.use(routes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
