8c7acc347bdbfd6b6d4cc13159dd4a8714241fec
Tyreeze <wedding5@msu.edu>
2022-03-12 01:44:03 -0600

    refactoring code

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -6,7 +6,7 @@ import { ADD_USER } from '../utils/mutations'
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
-  const [userData, setUserFormData] = useState({
+  const [userFormData, setUserFormData] = useState({
     username: '',
     email: '',
     password: '',
